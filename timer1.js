527cf2bd1dc5edb689cc91019d5713820bd38604
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-22 23:12:08 +0000

    Refactor for linting

--- timer1.js
+++ timer1.js
@@ -1,9 +1,9 @@
 let numArguments = process.argv.length;
 
 if (numArguments > 2) {
-  const arguments = process.argv.slice(2, numArguments);
+  const inputArguments = process.argv.slice(2, numArguments);
 
-  for (const argument of arguments) {
+  for (const argument of inputArguments) {
     const delay = Number(argument) * 1000;
 
     if (!isNaN(delay) && delay >= 0) {
